ec499fe9133010401282e195a3c7f7fe13b4ac1d
Rhushikesh Lokhande <rhushikeshl@byteridge.com>
2020-02-18 00:14:21 +0530

    added port

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var finalhandler = require('finalhandler');
 var serveStatic = require('serve-static');
 
 var serve = serveStatic("./");
-
+const port = process.env.PORT || 8000;
 var server = http.createServer(function(req, res) {
     var done = finalhandler(req, res);
     serve(req, res, done);
 });
 
-server.listen(8000);
+server.listen(port);
